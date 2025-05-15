c3f7dbf2efc20b81f5f5a62fd71bdfd883e98275
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-05-15 11:27:10 +0100

    Import expand class

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,9 @@ import '../styles/main.scss';
 import Header from './Header';
 import TabsCollection from './Tabs';
 import VideoPlayerCollection from './VideoPlayer';
+import ExpandableContentCollection from './ExpandableContent';
 
 new Header();
 new TabsCollection();
 new VideoPlayerCollection();
+new ExpandableContentCollection();
