3e0e87bd8b04080cdf88456d9f214b12b2c2aac7
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-05-22 15:38:23 +0100

    Implement custom select

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,11 @@ import TabsCollection from './Tabs';
 import VideoPlayerCollection from './VideoPlayer';
 import ExpandableContentCollection from './ExpandableContent';
 import InputMaskCollection from './InputMask';
+import SelectCollection from './Select';
 
 new Header();
 new TabsCollection();
 new VideoPlayerCollection();
 new ExpandableContentCollection();
 new InputMaskCollection();
+new SelectCollection();
