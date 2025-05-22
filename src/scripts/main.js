fba17100bcbc216868ad6f9b77b26e00295eea44
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-05-22 12:50:17 +0100

    Implement imask

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,10 @@ import Header from './Header';
 import TabsCollection from './Tabs';
 import VideoPlayerCollection from './VideoPlayer';
 import ExpandableContentCollection from './ExpandableContent';
+import InputMaskCollection from './InputMask';
 
 new Header();
 new TabsCollection();
 new VideoPlayerCollection();
 new ExpandableContentCollection();
+new InputMaskCollection();
